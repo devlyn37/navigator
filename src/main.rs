4f4b9a668ca4b0b4232a4bc2e64a3f56c2630f81
Devlyn Dorfer <devlyndorfer@gmail.com>
2022-12-27 01:43:36 -0500

    use ethers, fetch an abi

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,25 @@
-use std::{
-    fs::File,
-    io::{BufRead, BufReader},
-};
-
-use anyhow::{Context, Result};
+use anyhow::Result;
 use clap::Parser;
-
+use ethers::{etherscan::Client, types::Chain};
 /// Search for a pattern in a file and display th elines that contain it.
 #[derive(Parser)]
 struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    path: std::path::PathBuf,
+    contract_address: String,
+		network: String
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Cli::parse();
-    println!("pattern: {:?}", args.pattern);
-    println!("path: {:?}", args.path);
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+		let args = Cli::parse();
+		println!("contract address: {:?}", args.contract_address);
+		println!("network: {:?}", args.network);
+		let api_key = "K14P3TW12QCI2VDR3YIDY7XA9Y5XP2D232";
+		let client = Client::new(Chain::Mainnet, api_key).unwrap();
+    let abi = client
+        .contract_abi(args.contract_address.parse().unwrap()).await?;
 
-    let file =
-        File::open(&args.path).with_context(|| format!("Could not read file `{:?}`", args.path))?;
-    let reader = BufReader::new(file);
-
-    for line in reader.lines() {
-        let validated_line =
-            line.with_context(|| format!("Could not read line from file `{:?}`", args.path))?;
-
-        if validated_line.contains(&args.pattern) {
-            println!("{:?}", validated_line);
-        }
-    }
+		let events = abi.events;
+		println!("hi: {:?}", events);
 
     Ok(())
 }
