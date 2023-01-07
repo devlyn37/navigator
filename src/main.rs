3d0cdb1dc9fd29b0dac0b2f3384cad8c7f37aa58
Devlyn Dorfer <devlyndorfer@gmail.com>
2023-01-07 12:44:46 -0500

    Clean up and format

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,69 @@
-use std::{str::FromStr};
+use std::str::FromStr;
 
 use anyhow::Result;
 use clap::Parser;
-use ethers::{prelude::*, utils::hex, abi::Token};
+use ethers::{
+    abi::{ethabi::AbiError, Token},
+    prelude::*,
+    utils::hex,
+};
 /// Search for a pattern in a file and display th elines that contain it.
 #[derive(Parser)]
 struct Cli {
     contract_address: String,
-		chain: String,
-		error: String,
-		etherscan_key: String,
+    chain: String,
+    error: String,
+    etherscan_key: String,
 }
-
-// 0xd2ade556
-// 0x98AA442ceFCAF0A7277D10889d07d04E90B37eA5
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-		let args = Cli::parse();
-		println!("contract address: {}, chain: {}, error: {}, key: {}", args.contract_address, args.chain, args.error, args.etherscan_key);
-		let address: Address = args.contract_address.parse().expect("Provided contract address invalid");
-		let chain = Chain::from_str(&args.chain).expect("Provided chain name invalid");
-		let client = Client::new(chain, args.etherscan_key).unwrap();
+    let args = Cli::parse();
+    println!(
+        "contract address: {}, chain: {}, error: {}, key: {}",
+        args.contract_address, args.chain, args.error, args.etherscan_key
+    );
+    let address: Address = args
+        .contract_address
+        .parse()
+        .expect("Provided contract address invalid");
+    let chain = Chain::from_str(&args.chain).expect("Provided chain name invalid");
+    let client = Client::new(chain, args.etherscan_key).unwrap();
 
-		println!("Fetching on abi for contract on {} on chain {}", args.contract_address, args.chain);
+    println!(
+        "Fetching on abi for contract on {} on chain {}",
+        args.contract_address, args.chain
+    );
     let abi = client
-        .contract_abi(address).await.expect("Could not fetch the abi for the provided contract address");
-		let (error_name, args) = parse_error(abi, &args.error).expect("Could not decode error with provided information");
+        .contract_abi(address)
+        .await
+        .expect("Could not fetch the abi for the provided contract address");
+    let (error_name, args) =
+        parse_error(abi, &args.error).expect("Could not decode error with provided information");
 
-		println!("Error name: {}", error_name);
-		println!("Args: {:?}", args);
+    println!("Error name: {}", error_name);
+    println!("Args: {:?}", args);
 
     Ok(())
 }
 
 fn parse_error(contract: ethers::abi::Contract, hex: &str) -> Result<(String, Vec<Token>), String> {
-	for errors in contract.errors.into_values() {
-			let error = &errors[0];
-			let encoded_signature = hex::encode(error.signature().as_bytes());
-			if encoded_signature.contains(hex.trim_start_matches("0x")) {
-				let decoded = error.decode(hex.as_bytes());
-				match decoded {
-						Ok(result) => return Ok((error.name.to_owned(), result)),
-						Err(_) => return Err("Could not decode data".to_string()),
-				}
-			}
-	}
+    let errors: Vec<AbiError> = contract
+        .errors
+        .into_values()
+        .filter_map(|x| x.into_iter().nth(0))
+        .collect();
 
-	return Err("No errors within the contract mapped to the data provided".to_string());
+    for error in errors {
+        let encoded_signature = hex::encode(error.signature().as_bytes());
+        if encoded_signature.contains(hex.trim_start_matches("0x")) {
+            let decoded = error.decode(hex.as_bytes());
+            match decoded {
+                Ok(result) => return Ok((error.name.to_owned(), result)),
+                Err(_) => return Err("Could not decode data".to_string()),
+            }
+        }
+    }
+
+    return Err("No errors within the contract mapped to the data provided".to_string());
 }
