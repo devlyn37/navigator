4f9b46e0956aba3e90c212f5dba5e405eac1bf9c
Devlyn Dorfer <devlyndorfer@gmail.com>
2023-01-07 11:09:46 -0500

    Improve error handling a bit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use core::panic;
 use std::{str::FromStr};
 
 use anyhow::Result;
@@ -20,29 +19,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 		let args = Cli::parse();
 		println!("contract address: {}, chain: {}, error: {}", args.contract_address, args.chain, args.error);
 		let api_key = "K14P3TW12QCI2VDR3YIDY7XA9Y5XP2D232";
-		let client = Client::new(Chain::from_str(&args.chain).unwrap(), api_key).unwrap();
+		let address: Address = args.contract_address.parse().expect("Provided contract address invalid");
+		let chain = Chain::from_str(&args.chain).expect("Provided chain name invalid");
+		let client = Client::new(chain, api_key).unwrap();
 
 		println!("Fetching on abi for contract on {} on chain {}", args.contract_address, args.chain);
     let abi = client
-        .contract_abi(args.contract_address.parse().unwrap()).await.expect("ahh");
+        .contract_abi(address).await.expect("Could not fetch the abi for the provided contract address");
+		let (error_name, args) = parse_error(abi, &args.error).expect("Could not decode error with provided information");
 
-		let (error_name, args) = parse_error(abi, &args.error);
 		println!("Error name: {}", error_name);
 		println!("Args: {:?}", args);
+
     Ok(())
 }
 
-fn parse_error(contract: ethers::abi::Contract, hex: &str) -> (String, Vec<Token>) {
-	for (_, list) in contract.errors.into_iter() {
-			let error = &list[0];
-			let error_signature = hex::encode(error.signature().as_bytes());
-			if error_signature.contains(hex.trim_start_matches("0x")) {
-				match error.decode(hex.as_bytes()) {
-						Ok(result) => return (error.name.to_owned(), result),
-						Err(_) => panic!("ahhh"),
-				} 
+fn parse_error(contract: ethers::abi::Contract, hex: &str) -> Result<(String, Vec<Token>), String> {
+	for errors in contract.errors.into_values() {
+			let error = &errors[0];
+			let encoded_signature = hex::encode(error.signature().as_bytes());
+			if encoded_signature.contains(hex.trim_start_matches("0x")) {
+				let decoded = error.decode(hex.as_bytes());
+				match decoded {
+						Ok(result) => return Ok((error.name.to_owned(), result)),
+						Err(_) => return Err("Could not decode data".to_string()),
+				}
 			}
 	}
 
-	panic!("ahh");
+	return Err("No errors within the contract mapped to the data provided".to_string());
 }
