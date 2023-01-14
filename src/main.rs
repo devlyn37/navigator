ccf79c302195bcff7d4a742f3b88be9fc2d62ab1
Devlyn Dorfer <devlyndorfer@gmail.com>
2023-01-14 11:33:04 -0500

    start adding sub commands

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::str::FromStr;
+use std::{ffi::OsString, str::FromStr};
 
 use anyhow::Result;
-use clap::Parser;
+use clap::{arg, ArgMatches, Command, Parser};
 use ethers::{
     abi::Token,
     prelude::*,
-    utils::{hex, keccak256},
+    utils::{hex},
 };
 /// Search for a pattern in a file and display th elines that contain it.
 #[derive(Parser)]
@@ -29,46 +29,6 @@ struct Cli {
 // 0xd2ade556
 // 0x98AA442ceFCAF0A7277D10889d07d04E90B37eA5
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Cli::parse();
-    println!(
-        "contract address: {}, chain: {}, data: {}, key: {}",
-        args.contract_address, args.chain, args.data, args.etherscan_key
-    );
-
-    let data = hex::decode(args.data.trim_start_matches("0x")).expect("data provided is not hex");
-    let address: Address = args
-        .contract_address
-        .parse()
-        .expect("Provided contract address invalid");
-    let chain = Chain::from_str(&args.chain).expect("Provided chain name invalid");
-    let client = Client::new(chain, args.etherscan_key).unwrap();
-
-    println!(
-        "Here's the hash of the error message {}",
-        hex::encode(keccak256("SeasonPassNFT: Not enough ETH sent"))
-    );
-
-    println!(
-        "Fetching on abi for contract on {} on chain {}",
-        args.contract_address, args.chain
-    );
-    let abi = client
-        .contract_abi(address)
-        .await
-        .expect("Could not fetch the abi for the provided contract address");
-    let (name, args) =
-        parse_error(abi, data).expect("Could not decode error with provided information");
-    // let (name, args) =
-    //     parse_function(abi, data).expect("Could not decode error with provided information");
-
-    println!("Error name: {}", name);
-    println!("Args: {:?}", args);
-
-    Ok(())
-}
-
 fn parse_error(contract: ethers::abi::Contract, data: Vec<u8>) -> Option<(String, Vec<Token>)> {
     let found = contract
         .errors
@@ -76,11 +36,11 @@ fn parse_error(contract: ethers::abi::Contract, data: Vec<u8>) -> Option<(String
         .filter_map(|x| x.into_iter().nth(0))
         .find(|error| {
             let error_signature = &error.signature().to_fixed_bytes()[0..4];
-						let data_signature = &data[0..4];
+            let data_signature = &data[0..4];
             return error_signature == data_signature;
         })?;
 
-		let params = &data[4..];
+    let params = &data[4..];
     let decoded = found.decode(params).ok()?;
     Some((found.name.to_owned(), decoded))
 }
@@ -91,11 +51,106 @@ fn parse_function(contract: ethers::abi::Contract, data: Vec<u8>) -> Option<(Str
         .into_values()
         .filter_map(|x| x.into_iter().nth(0))
         .find(|function| {
-						let signature = &data[0..4];
+            let signature = &data[0..4];
             return signature == function.short_signature();
         })?;
 
-		let params = &data[4..];
+    let params = &data[4..];
     let decoded = found.decode_input(params).ok()?;
     Some((found.name.to_owned(), decoded))
+}
+
+fn cli() -> Command {
+    Command::new("git")
+        .about("A fictional versioning CLI")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_external_subcommands(true)
+        .subcommand(
+            Command::new("decode")
+                .about("decodes hex ethereum call data")
+                .subcommand_required(true)
+                .arg_required_else_help(true)
+                .subcommand(
+                    Command::new("error")
+                        .about("decodes solidity custom error data")
+                        .arg_required_else_help(true)
+                        .arg(arg!(<CHAIN> "The remote to target"))
+                        .arg(arg!(<CONTRACT> "contract address for the target"))
+                        .arg(arg!(<ETHERSCAN_KEY> "api key for etherscan"))
+                        .arg(arg!(<DATA> "the data to decode")),
+                )
+                .subcommand(
+                    Command::new("function")
+                        .about("decodes solidity function call data")
+                        .arg_required_else_help(true)
+                        .arg(arg!(<CHAIN> "The remote to target"))
+                        .arg(arg!(<CONTRACT> "contract address for the target"))
+                        .arg(arg!(<ETHERSCAN_KEY> "api key for etherscan"))
+                        .arg(arg!(<DATA> "the data to decode")),
+                ),
+        )
+}
+
+fn validate_and_format_input(matches: &ArgMatches) -> (Chain, Address, String, Vec<u8>) {
+    let chain_input = matches.get_one::<String>("CHAIN").expect("required");
+    let contract_input = matches.get_one::<String>("CONTRACT").expect("required");
+    let key_input = matches
+        .get_one::<String>("ETHERSCAN_KEY")
+        .expect("required");
+    let data_input = matches.get_one::<String>("DATA").expect("required");
+
+    (
+        Chain::from_str(chain_input).expect("Provided chain name invalid"),
+        contract_input
+            .parse()
+            .expect("Provided contract address invalid"),
+        key_input.to_owned(),
+        hex::decode(data_input.trim_start_matches("0x")).expect("data provided is not hex"),
+    )
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let matches = cli().get_matches();
+    match matches.subcommand() {
+        Some(("decode", sub_matches)) => {
+            let decode_command = sub_matches.subcommand().unwrap_or(("decode", sub_matches));
+            match decode_command {
+                ("error", sub_matches) => {
+                    let (chain, contract_address, key, data) =
+                        validate_and_format_input(sub_matches);
+                    let client = Client::new(chain, key).unwrap();
+                    let abi = client
+                        .contract_abi(contract_address)
+                        .await
+                        .expect("Could not fetch the abi for the provided contract address");
+                    let (name, args) = parse_error(abi, data).expect("ahhl");
+                    println!("Error name: {}", name);
+                    println!("Args: {:?}", args);
+                    return Ok(());
+                }
+                (ext, sub_matches) => {
+                    let args = sub_matches
+                        .get_many::<OsString>("")
+                        .into_iter()
+                        .flatten()
+                        .collect::<Vec<_>>();
+                    println!("Calling out to {:?} with {:?}", ext, args);
+                    return Ok(());
+                }
+                _ => unreachable!(), // If all subcommands are defined above, anything else is unreachabe!()
+            };
+        }
+        Some((ext, sub_matches)) => {
+            let args = sub_matches
+                .get_many::<OsString>("")
+                .into_iter()
+                .flatten()
+                .collect::<Vec<_>>();
+            println!("Calling out to {:?} with {:?}", ext, args);
+            Ok(())
+        }
+        _ => unreachable!(), // If all subcommands are defined above, anything else is unreachabe!()
+    }
 }
