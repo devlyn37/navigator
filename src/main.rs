3d336674ef16da0adc7dcb7ad1da45845a0fe1a5
Devlyn Dorfer <devlyndorfer@gmail.com>
2023-01-13 21:51:52 -0500

    clean up error parsing

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,18 @@ struct Cli {
     etherscan_key: String,
 }
 
+// function test
+
 // contract address:
 // 0x1d9317911cf1003b42a965574c29f18a87a2858c
 // data:
 // 0x0209c6b7000000000000000000000000292f9d08efcf1a3a988959190d44f48a53577f100000000000000000000000000000000000000000000000000000000000000001
+
+// error test
+
+// goerli
+// 0xd2ade556
+// 0x98AA442ceFCAF0A7277D10889d07d04E90B37eA5
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -50,30 +58,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .contract_abi(address)
         .await
         .expect("Could not fetch the abi for the provided contract address");
-    // let (error_name, args) =
-    //     parse_error(abi, &args.error).expect("Could not decode error with provided information");
-    let (function_name, args) =
-        parse_function(abi, data).expect("Could not decode error with provided information");
+    let (name, args) =
+        parse_error(abi, data).expect("Could not decode error with provided information");
+    // let (name, args) =
+    //     parse_function(abi, data).expect("Could not decode error with provided information");
 
-    println!("Error name: {}", function_name);
+    println!("Error name: {}", name);
     println!("Args: {:?}", args);
 
     Ok(())
 }
 
-// fn parse_error(contract: ethers::abi::Contract, data: &Vec<u8>) -> Option<(String, Vec<Token>)> {
-//     let found = contract
-//         .errors
-//         .into_values()
-//         .filter_map(|x| x.into_iter().nth(0))
-//         .find(|error| {
-//             let encoded_signature = hex::encode(error.signature());
-//             return encoded_signature.contains(hex.trim_start_matches("0x"));
-//         })?;
+fn parse_error(contract: ethers::abi::Contract, data: Vec<u8>) -> Option<(String, Vec<Token>)> {
+    let found = contract
+        .errors
+        .into_values()
+        .filter_map(|x| x.into_iter().nth(0))
+        .find(|error| {
+            let error_signature = &error.signature().to_fixed_bytes()[0..4];
+						let data_signature = &data[0..4];
+            return error_signature == data_signature;
+        })?;
 
-//     let decoded = found.decode(hex.as_bytes()).ok()?;
-//     Some((found.name.to_owned(), decoded))
-// }
+		let params = &data[4..];
+    let decoded = found.decode(params).ok()?;
+    Some((found.name.to_owned(), decoded))
+}
 
 fn parse_function(contract: ethers::abi::Contract, data: Vec<u8>) -> Option<(String, Vec<Token>)> {
     let found = contract
